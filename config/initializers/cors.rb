ab8692256894233116ced8caf3bd702de8694fbe
elviajero971 <lucas.illiano@hotmail.com>
2022-08-24 13:38:58 +0200

    cors any

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '/tennis_players', headers: :any, methods: [:get]
+    resource '*', headers: :any, methods: [:get]
   end
 end
